e2c24830c7184c1234bf4bd64a99209cd67cc04e
nawendusingh <nawendu6.72f@gmail.com>
2020-02-04 23:54:25 -0700

    seats done

--- seat-booking/script.js
+++ seat-booking/script.js
@@ -1,3 +1,23 @@
+// SELECTION OF DOM
 const container = document.querySelector(".container");
-// select all seats and put in an array
-const seats = document.querySelectorAll(".seat");
+// select all seats in the row that donot have occupied class and put in an array
+const seats = document.querySelectorAll(".row .seat:not(.occupied");
+// get the spans
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+// getthe movie price and convert to int
+const ticketPrice = +movieSelect.value;
+
+// EVENT LISTENER
+container.addEventListener("click", e => {
+  if (
+    //   if the list of class has seat
+    e.target.classList.contains("seat") &&
+    // and not occupied
+    !e.target.classList.contains("occupied")
+  ) {
+    //   toggle the css class selected [other options are add remove]
+    e.target.classList.toggle("selected");
+  }
+});
